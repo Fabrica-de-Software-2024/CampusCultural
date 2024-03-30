e546d830b4281697b2d578729748f8a75bfd1226
Caio Ramires <102688227+ramires994@users.noreply.github.com>
2024-03-30 15:31:05 -0300

    Ajuste de app.module.ts

--- CampusCulturalBack/src/app.module.ts
+++ CampusCulturalBack/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { UsuarioModule } from './module/usuario/usuario.module';
 import { EventoModule } from './module/evento/evento.module';
 import { EventoInscricaoModule } from './module/evento_inscricao/evento_inscricao.module';
-import { EventoInscricaoModule } from './module/evento_inscricao/evento_inscricao.module';
 
 @Module({
   imports: [UsuarioModule, EventoModule, EventoInscricaoModule],
