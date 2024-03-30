7a3f5fdaa473712e887fad11477f34f7d2235f08
Caio Ramires <102688227+ramires994@users.noreply.github.com>
2024-03-30 15:41:20 -0300

    Reajuste de evento_inscricao.dolute.ts

--- CampusCulturalBack/src/module/evento_inscricao/evento_inscricao.module.ts
+++ CampusCulturalBack/src/module/evento_inscricao/evento_inscricao.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EventoInscricaoService } from './evento_inscricao.service';
 import { EventoInscricaoController } from './evento_inscricao.controller';
-import { PrismaService } from 'src/database/PrismaService';
+import { PrismaService } from '../../database/PrismaService';
 
 @Module({
   controllers: [EventoInscricaoController],
