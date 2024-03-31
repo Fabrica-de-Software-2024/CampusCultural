316d844ef51333c24ae74003736ac26a3c3404a9
Caiki <caiki2003@gmail.com>
2024-03-31 16:38:30 -0300

    ajustado problema de CSS no Swagger

--- CampusCulturalBack/src/main.ts
+++ CampusCulturalBack/src/main.ts
@@ -17,7 +17,9 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document, {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+  });
 
 
   await app.listen(3000);
