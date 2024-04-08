1315f8f5aba16e0ac34e24f77fa025ae5f5c3a2e
Caiki <caiki2003@gmail.com>
2024-04-07 21:26:11 -0300

    header

--- CampusCulturalFront/app/index.tsx
+++ CampusCulturalFront/app/index.tsx
@@ -1,11 +1,15 @@
 import { router } from "expo-router"
 import { Button, Text } from "react-native"
 import Navbar from "./components/Navbar"
+import { useState } from "react";
 
 export default function Index(){
+
+    const [selecionado, setSelecionado] = useState(0);
+
     return(
         <>
-            <Navbar />
+            <Navbar title={"Início"} selecionado={selecionado} setSelecionado={setSelecionado} />
             <Text>teste</Text>
             <Button title="login" onPress={()=>router.navigate("/login")}></Button>
         </>
