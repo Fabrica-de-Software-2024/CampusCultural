16e46d707f4e3a4d3b6ec453ebdcabe6467b09bb
Lucas Santos <devlucassantos@icloud.com>
2024-04-08 01:35:50 -0300

    Update perfil.tsx

--- CampusCulturalFront/app/perfil.tsx
+++ CampusCulturalFront/app/perfil.tsx
@@ -14,13 +14,17 @@ const Perfil = () => {
     <>
       <Navbar title="Perfil" links={false} />
       <View style={styles.container}>
-        <Image source={{ uri: userData.photo }} style={styles.photo} />
-        <Text style={styles.name}>{userData.name}</Text>
-        <Text style={styles.course}>{userData.course}</Text>
-        <Text style={styles.option}>Notificações</Text>
-        <Text style={styles.option}>Configurações</Text>
-        <Text style={styles.option}>Meus Certificados</Text>
-        <Text style={styles.option}>Fale Conosco</Text>
+        <View style={styles.userInfo}>
+          <Image source={{ uri: userData.photo }} style={styles.photo} />
+          <Text style={styles.name}>{userData.name}</Text>
+          <Text style={styles.course}>{userData.course}</Text>
+        </View>
+        <View style={styles.options}>
+          <Text style={styles.option}>Notificações</Text>
+          <Text style={styles.option}>Configurações</Text>
+          <Text style={styles.option}>Meus Certificados</Text>
+          <Text style={styles.option}>Fale Conosco</Text>
+        </View>
       </View>
       <Rodape selecionado={3} />
     </>
@@ -31,24 +35,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start", // Alinhar o conteúdo no topo
     backgroundColor: "#fff",
+  },
+  userInfo: {
+    alignItems: "center",
+    marginBottom: 20,
   },
   photo: {
     width: 150,
     height: 150,
     borderRadius: 75,
-    marginBottom: 20,
+    marginBottom: 10,
   },
   name: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 10,
+    marginBottom: 5,
   },
   course: {
     fontSize: 18,
     fontStyle: "italic",
     marginBottom: 10,
+  },
+  options: {
+    alignItems: "center",
   },
   option: {
     fontSize: 18,
