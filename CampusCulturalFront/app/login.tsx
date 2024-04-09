7a7ac81246a2aa17ced74a9217806bab4fccb2d1
AdrianaSarturi <146396758+AdrianaSarturi@users.noreply.github.com>
2024-04-08 22:12:23 -0300

    Add: Alterações tela login, tela de cadastro e adição de imagens

--- CampusCulturalFront/app/login.tsx
+++ CampusCulturalFront/app/login.tsx
@@ -4,29 +4,32 @@ import { Image, View, StyleSheet, Dimensions, Text, TextInput, TouchableOpacity 
 export default function Login() {
     const login = require("../assets/login.png");
     const logo = require("../assets/logo.png");
+    const figura1 = require("../assets/figura1.png");
+    const figura2 = require("../assets/figura2.png");
+    const figura3 = require("../assets/figura3.png");
+    const figura4 = require("../assets/figura4.png");
 
     return (
         <View style={styles.container}>
             <Image style={styles.imagem} source={login} resizeMode="cover" />
             <Image style={styles.logo} source={logo} />
-            <Image style={styles.logo} source={logo} />
+            <Image style={styles.figura1} source={figura1} resizeMode="cover" />
+            <Image style={styles.figura2} source={figura2} resizeMode="cover" />
+            <Image style={styles.figura3} source={figura3} resizeMode="cover" />
+            <Image style={styles.figura4} source={figura4} resizeMode="cover" />
+
             <View style={styles.bottomContainer} />
             <View style={styles.textContainer}>
                 <Text style={styles.purpleText}>BEM-VINDO(A) AO NOSSO</Text>
                 <Text style={styles.purpleText2}>CAMPUS CULTURAL!</Text>
-                <Text style={styles.grayText}>Quer ter acesso ao calendário de eventos em tempo real? <Text style={styles.purpleText3}>Efetue o login!</Text></Text>
+                <Text style={styles.grayText}>
+                    Quer ter acesso ao calendário de eventos em tempo real? <Text style={styles.purpleText3}>Efetue o login!</Text>
+                </Text>
                 <View style={styles.inputContainer}>
                     <Text style={styles.inputLabel}>e-mail:</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholderTextColor="#8A60FF"
-                    />
+                    <TextInput style={styles.input} placeholderTextColor="#8A60FF" />
                     <Text style={styles.inputLabel}>senha:</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholderTextColor="#8A60FF"
-                        secureTextEntry={true}
-                    />
+                    <TextInput style={styles.input} placeholderTextColor="#8A60FF" secureTextEntry={true} />
                     <View style={styles.buttonContainer}>
                         <TouchableOpacity style={styles.button}>
                             <Text style={styles.buttonText}>ENTRAR</Text>
@@ -41,7 +44,6 @@ export default function Login() {
 const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
-    //Container da imagem grande no topo
     container: {
         flex: 1,
         justifyContent: 'flex-start',
@@ -49,6 +51,8 @@ const styles = StyleSheet.create({
         backgroundColor: "#6B3BF4",
         paddingTop: 0,
         margin: 0,
+        width: '100%', // Garante que a view ocupe toda a largura
+        height: '100%', // Garante que a view ocupe toda a altura
     },
     //Imagem grande do topo
     imagem: {
@@ -59,10 +63,42 @@ const styles = StyleSheet.create({
     // Logo acima da imagem de login
     logo: {
         position: 'absolute',
-        alignSelf: 'center', 
+        alignSelf: 'center',
         top: windowHeight * 0.200 - 100, // Posição a partir do topo da tela
-        width: 200, 
-        height: 200, 
+        width: 190,
+        height: 190,
+    },
+    //Figura superior eaquerda
+    figura1: {
+        top: '0%',
+        left: '0%',
+        position: 'absolute',
+        width: 100,
+        height: 100,
+    },
+    //Figura superior direita
+    figura2: {
+        width: '30%',
+        top: '0%',
+        right: '0%',
+        position: 'absolute',
+    },
+    //Figura lateral direita
+    figura3: {
+        bottom: "10%",
+        right: 0,
+        position: 'absolute',
+
+        zIndex: 1,
+    },
+    //Figura inferior esquerda
+    figura4: {
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+        width: 200,
+        height: 200,
+        zIndex: 1,
     },
     //Container branco da parte inferior
     bottomContainer: {
@@ -78,7 +114,7 @@ const styles = StyleSheet.create({
     //Container envolvendo os textos
     textContainer: {
         position: 'absolute',
-        top: windowHeight * 0.45 - 35,
+        paddingTop: windowHeight * 0.40,
         alignItems: 'center',
     },
     //Definições para a frase "BEM-VINDO(A) AO NOSSO"
@@ -94,7 +130,6 @@ const styles = StyleSheet.create({
         color: '#6B3BF4',
         alignSelf: 'flex-start',
         marginLeft: 35,
-
     },
     //Definições para a frase "Quer ter acesso ao calendário de eventos em tempo real?"" 
     grayText: {
